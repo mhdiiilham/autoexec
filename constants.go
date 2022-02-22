6406d66c47326cf0d91b955925cf9459adb9c06a
Muhammad Ilham <muchamad.ilham@amartha.com>
2022-02-22 08:00:04 +0700

    feat: Declare Constants

---
+++ constants.go
@@ -0,0 +1,15 @@
+package autoexe
+
+const (
+	INVALID_ARG = 400
+
+	DEFAULT             = ""
+	DEFAULT_CONFIG      = "https://raw.githubusercontent.com/mhdiiilham/csgo-cfg/master/auto.cfg"
+	DEAULT_STEAMID32    = "416307248"
+	DEFAULT_CONFIG_NAME = "autoexec"
+
+	CONFIG_DIRECTORY = `%s\Steam\userdata\%s\730\local\cfg`
+	CSGO_GAME_CODE   = "730"
+	CSGO_FILE_DST    = "local" // need to rename this
+	CSGO_CFG_GILE    = "cfg"
+)
